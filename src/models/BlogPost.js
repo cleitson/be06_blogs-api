05fd32289bcc2e4c1e589b519ac6262cb18e233c
cleitson <cleitson.ftw@gmail.com>
2024-01-07 14:09:38 -0300

    model blogpost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,52 @@
+const BlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    updated: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id'
+      },
+      field: 'user_id',
+      foreignKey: true,
+      onDelete: 'SET NULL',
+      onUpdate: 'SET NULL'
+    }
+  }, {
+    tableName: 'blog_posts',
+    timeStamps: false,
+    underscored: true,
+  })
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user'
+    });
+
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPostModel;
